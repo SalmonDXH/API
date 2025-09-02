ce06f82c2308a57a1ebb9fcd0a2c265f8b7770d7
Salmon <salmonvn17@gmail.com>
2025-09-02 14:56:33 +0700

    My little api

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
 from fastapi import FastAPI
-import requests
 import os
-from dotenv import get_key, load_dotenv
+from dotenv import load_dotenv
 import base64
 from fastapi import FastAPI, Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
+from supabase import create_client, Client
 from slowapi.errors import RateLimitExceeded
 
 
 load_dotenv()
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN") 
-GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
-GITHUB_REPO = os.getenv("GITHUB_REPO")
-
-def decode_content(data):
-    if not data:
-        return ""
-    if data.get("encoding") == "base64":
-        return base64.b64decode(data["content"]).decode("utf-8")
-    return ""
-
-class Github:
-    def __init__(self,token, username, repo):
-        self.url = f"https://api.github.com/repos/{username}/{repo}/contents/"
-        self.token = token
-    
-    def getData(self, file):
-        urls = self.url + file
-        headers = {
-            "Authorization": f"token {self.token}",
-            "Accept": "application/vnd.github.v3+json"
-        }
-        response = requests.get(urls, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return None
+supaurl: str = os.environ.get("supaurl")
+supakey: str = os.environ.get("supakey")
+supabase: Client = create_client(supaurl, supakey)
 
 
 app = FastAPI()
@@ -46,26 +22,21 @@ app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
+
 @app.get("/")
 @limiter.limit("1/minute")
-def read_root(request: Request):  # <- add this
+def read_root(request: Request):  
     return {"message": "Hello World"}
 
 @app.get("/orca/{hwid}")
 @limiter.limit("10/minute")
 def gethwid_orca(hwid: str,request: Request):
-    git = Github(GITHUB_TOKEN,GITHUB_USERNAME,GITHUB_REPO)
-    content = decode_content(git.getData("advancehwid"))
-    splitted_line = content.split("\n")
-    hwids = []
-    for lines in splitted_line:
-        try:
-            hwids.append(lines.split("=")[0])
-        except:
-            continue
-    found = hwid in hwids
-    
-    result_string = "1" if found else "0"
+    result_string = "0"
+    try:
+        user_data = (supabase.table("Orca").select("username").eq("hwid", hwid).execute()).data[0]
+        result_string = "1"
+    except:
+        result_string = "0"
     encoded_result = base64.b64encode(result_string.encode("utf-8")).decode("utf-8")
     
     return {"result": encoded_result}
@@ -73,18 +44,25 @@ def gethwid_orca(hwid: str,request: Request):
 @app.get("/normal/{hwid}")
 @limiter.limit("10/minute")
 def gethwid_normal(hwid: str,request: Request):
-    git = Github(GITHUB_TOKEN,GITHUB_USERNAME,GITHUB_REPO)
-    content = decode_content(git.getData("hwid"))
-    splitted_line = content.split("\n")
-    hwids = []
-    for lines in splitted_line:
-        try:
-            hwids.append(lines.split("=")[0])
-        except:
-            continue
-    found = hwid in hwids
+    result_string = "0"
+    try:
+        user_data = (supabase.table("Donator").select("username").eq("hwid", hwid).execute()).data[0]
+        result_string = "1"
+    except:
+        result_string = "0"
+    encoded_result = base64.b64encode(result_string.encode("utf-8")).decode("utf-8")
     
-    result_string = "1" if found else "0"
+    return {"result": encoded_result}
+
+@app.get("/tester/{hwid}")
+@limiter.limit("10/minute")
+def gethwid_tester(hwid: str,request: Request):
+    result_string = "0"
+    try:
+        user_data = (supabase.table("Tester").select("username").eq("hwid", hwid).execute()).data[0]
+        result_string = "1"
+    except:
+        result_string = "0"
     encoded_result = base64.b64encode(result_string.encode("utf-8")).decode("utf-8")
     
     return {"result": encoded_result}
