f12fb1273cd6cd66ed897afdfb3c35a73bc545be
Salmon <90376520+SalmonDXH@users.noreply.github.com>
2025-11-06 16:58:54 +0700

    update api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
 import base64
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from supabase import create_client, Client
@@ -121,3 +121,12 @@ async def v2_state(request: Request):
         return {'result': res}
     except Exception as e:
         return {'result': 'Error'}
+
+@app.get('/api/statistic')
+@limiter.limit('10/minute')
+async def api_statistic(request: Request):
+    try:
+        result = (supabase.rpc('macro_statistic', {}).execute()).dict()['data'][0]
+        return result
+    except:
+        raise HTTPException(status_code=400, detail="Something went wrong")
